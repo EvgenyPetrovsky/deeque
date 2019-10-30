c8fbdfdd9fca7c7208caeada02fa298d53a22189
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2019-10-30 17:41:31 +0100

    move checks_to_df to reporting and add threshold functions

--- R/checks.R
+++ R/checks.R
@@ -32,14 +32,9 @@ run_check <- function(data, check) {
         function_name = check$function_name,
         check_column = check$parameters$column,
         scope_size = length(res),
-        valid_ratio = ratio(res)
+        valid_ratio = ratio(res),
+        check_success = ratio(res) == 1
     )
-}
-
-checks_to_data_frame <- function(group) {
-    group %>%
-        Reduce(f = rbind, init = list()) %>%
-        as.data.frame(stringsAsFactors = FALSE)
 }
 
 severity_rank <- function(severity) {
@@ -52,4 +47,53 @@ severity_rank <- function(severity) {
         f = function(z, x) {z + x * (severity == labels[x])},
         x = ranks, init = rep(0, length(ranks))
     )
+}
+
+#' Function that creates severity check function.
+#'
+#' Function takes only severity level parameter and returns function that
+#' accepts data check result and checks whether defined severity level was
+#' reached. If severity level was reached function returns TRUE.
+#'
+#' @export
+#'
+#' @param severity - severity text label (INFO, WARNING, ERROR)
+severity_at_threshold <- function(severity) {
+    function(check_results) {
+        check_sev_ranks <- severity_rank(check_results$severity)
+        input_sev_rank  <- severity_rank(severity)
+        filter_condition <- check_sev_ranks >= input_sev_rank & check_success == FALSE
+        violations_found <- nrow(check_results[filter_condition]) > 0
+        violations_found
+    }
+}
+
+#' Function that creates severity check function.
+#'
+#' @export
+#'
+#' @param severity - severity text label (INFO, WARNING, ERROR)
+severity_under_threshold <- function(severity) {
+    function(check_results) {
+        check_sev_ranks <- severity_rank(check_results$severity)
+        input_sev_rank  <- severity_rank(severity)
+        filter_condition <- check_sev_ranks >= input_sev_rank & check_success == FALSE
+        violations_found <- nrow(check_results[filter_condition]) > 0
+        violations_found
+    }
+}
+
+#' Function that creates severity check function.
+#'
+#' @export
+#'
+#' @param severity - severity text label (INFO, WARNING, ERROR)
+severity_above_threshold <- function(severity) {
+    function(check_results) {
+        check_sev_ranks <- severity_rank(check_results$severity)
+        input_sev_rank  <- severity_rank(severity)
+        filter_condition <- check_sev_ranks >= input_sev_rank & check_success == FALSE
+        violations_found <- nrow(check_results[filter_condition]) > 0
+        violations_found
+    }
 }
