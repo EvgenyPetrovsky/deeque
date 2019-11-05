f613a5da46cbb9151f5950a180376e141edd60fb
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2019-11-06 00:37:03 +0100

    fix conditions

--- R/runner.R
+++ R/runner.R
@@ -3,14 +3,14 @@
 #' Apply group of checks to dataset. Function returns check results. In case when 
 #' \code{stop_condition} is specified function stops execution when 
 #'
-#'
-#' 
+#' @param data dataframe to check
+#' @param checks group of checks created by new_group() %>% add_check() ...
 #' @param condition function that takes check_result 
 #'   and returns TRUE / FALSE where FALSE halts execution.
 run_checks <- function(data, checks, condition = NULL) {
     check_result <- Map(f = function(check) {apply_check(data, check)}, checks)
-    if (!is.null(stop_condition)) {
-        analyze_run(check_result, stop_condition)
+    if (!is.null(condition)) {
+        analyze_run(check_result, condition)
     } else {
         check_result
     }
@@ -24,12 +24,11 @@ run_checks <- function(data, checks, condition = NULL) {
 #'
 #' @export
 #'
-#' @param data dataset that needs to be checked
-#' @param checks group of checks that needs to be applied to dataset
+#' @param check_result list of check application outcomes (\code{apply_check})
 #' @param condition function that takes check_result and returns TRUE / FALSE
 #'   where TRUE means success and proceeds execution and FALSE halts execution
 analyze_run <- function(check_result, condition) {
-    if (!is.null(stop_udf) && stop_udf(check_result)) {
+    if (!is.null(condition) & condition(check_result) == FALSE) {
         stop(paste("data quality check results are not sufficient to proceed"))
     } else {
         check_result
