20651c76765d5a068bedbb4311edf61315f8f2af
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2019-11-12 20:12:00 +0100

    add tests

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -13,3 +13,30 @@ test_that("severity_rank returns as many values as receives", {
     expect_equal(length(r), i)
   }
 })
+
+test_that("New group of checks is an empty list of length 0", {
+  g <- new_group()
+  expect_equal(length(g), 0)
+  expect_equal(g, list())
+})
+
+test_that("Function new check returns list object", {
+  c <- new_check(
+    description = "New check for testing", 
+    severity = severity[[1]], 
+    function_name = identity
+    )
+  expect_equal(is.list(c), TRUE)
+})
+
+#test_that("function new check fails if there is no description provided", {})
+
+#test_that("Function new_check fails if there is no severity defined or severity is invalid", {})
+
+#test_that("Function new_check fails if function is not defined") ???
+
+#test_that("Function new check returns object with elements 'description', 'severity', 'function_name', 'parameters'", {})
+
+#test_that("Function add_ccheck increases number of elements in group by 1")
+
+#test_that("only check can be included into group")
---
+++ tests/testthat/test-functions-table.R
@@ -0,0 +1,89 @@
+# tab_hasRowCount
+test_that("Empty dataframe has 0 rows", {
+  tab <- data.frame()
+  expect_equal(tab_hasRowCount(tab, udf = function(x) {x == 0}), TRUE)
+  expect_equal(tab_hasRowCount(tab, udf = function(x) {x != 0}), FALSE)
+})
+test_that("udf functoin works correct for number of rows check", {
+  for (i in sample.int(1000, 5)) {
+    tab <- data.frame(col = replicate(i, 42))
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x == i}), TRUE)
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x != i}), FALSE)
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x > i}), FALSE)
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x < i}), FALSE)
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x >= i}), TRUE)
+    expect_equal(tab_hasRowCount(tab, udf = function(x) {x <= i}), TRUE)
+  }
+})
+
+# tab_hasColumnCount
+test_that("empty dataframe has 0 columns", {
+  tab <- data.frame()
+  expect_equal(tab_hasColumnCount(tab, udf = function(x) {x == 0}), TRUE)
+  expect_equal(tab_hasColumnCount(tab, udf = function(x) {x != 0}), FALSE)
+})
+
+test_that("udf functoin works correct for number of columns check", {
+  for (i in sample.int(100, 5)) {
+    tab <- data.frame(Map(f = function(x) {1}, 1:i))
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x == i}), TRUE)
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x != i}), FALSE)
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x > i}), FALSE)
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x < i}), FALSE)
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x >= i}), TRUE)
+    expect_equal(tab_hasColumnCount(tab, udf = function(x) {x <= i}), TRUE)
+  }
+})
+
+# tab_hasColumn
+test_that("tab_hasColumn check for empty dataframe always returns FALSE", {
+  tab <- data.frame()
+  expect_equal(tab_hasColumn(tab, "A"), FALSE)
+  expect_equal(tab_hasColumn(tab, "B"), FALSE)
+})
+
+test_that("tab_hasColumn check returns TRUE when column exists", {
+  tab <- data.frame(a = 1, b = 1)
+  expect_equal(tab_hasColumn(tab, "a"), TRUE)
+  expect_equal(tab_hasColumn(tab, "b"), TRUE)
+})
+
+test_that("tab_hasColumn check returns FALSE when column exists", {
+  tab <- data.frame(a = 1, b = 1)
+  expect_equal(tab_hasColumn(tab, "A"), FALSE)
+  expect_equal(tab_hasColumn(tab, "B"), FALSE)
+})
+
+# tab_hasColumns
+test_that("tab_hasColumns check for empty dataframe always returns FALSE", {
+  tab <- data.frame()
+  expect_equal(tab_hasColumns(tab, "A"), FALSE)
+  expect_equal(tab_hasColumns(tab, "B"), FALSE)
+  expect_equal(tab_hasColumns(tab, c("A", "B")), FALSE)
+})
+
+test_that("tab_hasColumns check returns TRUE when all columns exist", {
+  tab <- data.frame(a = 1, b = 1, c = 1)
+  expect_equal(tab_hasColumns(tab, "a"), TRUE)
+  expect_equal(tab_hasColumns(tab, c("a", "a", "a")), TRUE)
+  expect_equal(tab_hasColumns(tab, "b"), TRUE)
+  expect_equal(tab_hasColumns(tab, c("a", "b")), TRUE)
+  expect_equal(tab_hasColumns(tab, c("a", "b", "c")), TRUE)
+})
+
+test_that("tab_hasColumns check returns FALSE when any of columns does not exist", {
+  tab <- data.frame(a = 1, b = 1, c = 1)
+  expect_equal(tab_hasColumns(tab, "A"), FALSE)
+  expect_equal(tab_hasColumns(tab, c("A", "A", "A")), FALSE)
+  expect_equal(tab_hasColumns(tab, "B"), FALSE)
+  expect_equal(tab_hasColumns(tab, c("a", "B")), FALSE)
+  expect_equal(tab_hasColumns(tab, c("a", "b", "c", "A")), FALSE)
+})
+
+# tab_hasUniqueKey
+test_that("tab_hasUniqueKey stops when dataframe has no columns", {
+  tab <- data.frame()
+  uk1 <- "uk_col_1"
+  err_message <- paste0(uk1, ".*", "missing")
+  expect_error(tab_hasUniqueKey(tab, uk1), )
+})
