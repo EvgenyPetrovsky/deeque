3961bc480db1ef3e2e3d7c37d9f1fa23e78de3d4
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2019-11-19 18:15:50 +0100

    test validation helpers

--- R/validation-helpers.R
+++ R/validation-helpers.R
@@ -39,20 +39,30 @@ udf_lt <- function(x) {
 #' @export
 #' @param x - value to compare with.
 udf_le <- function(x) {
-    function(value) {value < x}
+    function(value) {value <= x}
 }
 
+#' Between function
+#'
+#' Function returns another function that checks if given value is in between
+#' \code{x1} and \code{x2}
+#'
+#' @export
+#' @param x1 - left border
+#' @param x2 - right border
+#' @param include - inclusion type (0 / none, 1 / left, 2 / right, 3 / both) 
+#'   can be specified with digit, words, first letter of words
 udf_between <- function(
     x1, x2,
-    include = c("both", "right", "left", "none", "b", "r", "l", "n", 3, 2, 1, 0)
+    include = "both"
 ) {
     if (include %in% c("none", "n", 0)) {
         function(value) {x1 < value & value < x2}
-    } else if (include %in% c("right", "r", 1)) {
-        function(value) {x1 < value & value <= x2}
-    } else if (include %in% c("right", "r", 1)) {
+    } else if (include %in% c("right", "l", 1)) {
         function(value) {x1 <= value & value < x2}
-    } else if (include %in% c("both", "b", 1)) {
+    } else if (include %in% c("right", "r", 2)) {
+        function(value) {x1 < value & value <= x2}
+    } else if (include %in% c("both", "b", 3)) {
         function(value) {x1 <= value & value <= x2}
     } else {
         function(value) {FALSE}
---
+++ tests/testthat/test-validation-helpers.R
@@ -0,0 +1,161 @@
+# udf_gt
+test_that("'Greater than' function generates another function", {
+  expect_equal(typeof(udf_gt(3)), "closure")
+})
+
+test_that("'Greater than' function generates another function that takes one parameter", {
+  x <- 3
+  f <- udf_gt(x)
+  expect_equal(length(formals(f)), 1)
+})
+
+test_that("'Greater than' function generates another function that returns boolean result", {
+  x <- 3
+  f <- udf_gt(x)
+  expect_equal(typeof(f(x)), "logical")
+})
+
+test_that("'Greater than' function generates another function that works as expected", {
+  x <- 3
+  f <- udf_gt(x)
+  expect_equal(f(x - 1), FALSE)
+  expect_equal(f(x), FALSE)
+  expect_equal(f(x + 1), TRUE)
+})
+
+# udf_ge
+test_that("'Greater than or Equal to' function generates another function", {
+  expect_equal(typeof(udf_ge(3)), "closure")
+})
+
+test_that("'Greater than or Equal to' function generates another function that takes one parameter", {
+  x <- 3
+  f <- udf_ge(x)
+  expect_equal(length(formals(f)), 1)
+})
+
+test_that("'Greater than or Equal to' function generates another function that returns boolean result", {
+  x <- 3
+  f <- udf_ge(x)
+  expect_equal(typeof(f(x)), "logical")
+})
+
+test_that("'Greater than or Equal to' function generates another function that works as expected", {
+  x <- 3
+  f <- udf_ge(x)
+  expect_equal(f(x - 1), FALSE)
+  expect_equal(f(x), TRUE)
+  expect_equal(f(x + 1), TRUE)
+})
+
+# udf_lt
+test_that("'Less than' function generates another function", {
+  expect_equal(typeof(udf_lt(3)), "closure")
+})
+
+test_that("'Less than' function generates another function that takes one parameter", {
+  x <- 3
+  f <- udf_lt(x)
+  expect_equal(length(formals(f)), 1)
+})
+
+test_that("'Less than' function generates another function that returns boolean result", {
+  x <- 3
+  f <- udf_lt(x)
+  expect_equal(typeof(f(x)), "logical")
+})
+
+test_that("'Less than' function generates another function that works as expected", {
+  x <- 3
+  f <- udf_lt(x)
+  expect_equal(f(x - 1), TRUE)
+  expect_equal(f(x), FALSE)
+  expect_equal(f(x + 1), FALSE)
+})
+
+# udf_le
+test_that("'Less than or Equal to' function generates another function", {
+  expect_equal(typeof(udf_le(3)), "closure")
+})
+
+test_that("'Less than or Equal to' function generates another function that takes one parameter", {
+  x <- 3
+  f <- udf_le(x)
+  expect_equal(length(formals(f)), 1)
+})
+
+test_that("'Less than or Equal to' function generates another function that returns boolean result", {
+  x <- 3
+  f <- udf_le(x)
+  expect_equal(typeof(f(x)), "logical")
+})
+
+test_that("'Less than or Equal to' function generates another function that works as expected", {
+  x <- 3
+  f <- udf_le(x)
+  expect_equal(f(x - 1), TRUE)
+  expect_equal(f(x), TRUE)
+  expect_equal(f(x + 1), FALSE)
+})
+
+# udf_between
+test_that("'Between' function generates another function", {
+  x1 <- 3
+  x2 <- 6
+  expect_equal(typeof(udf_between(x1, x2, "b")), "closure")
+})
+
+test_that("'Between' function generates another function that takes one parameter", {
+  x1 <- 3
+  x2 <- 6
+  f1 <- udf_between(x1, x2)
+  f2 <- udf_between(x1, x2, "b")
+  expect_equal(length(formals(f1)), 1)
+  expect_equal(length(formals(f2)), 1)
+})
+
+test_that("'Between' function generates another function that returns boolean result", {
+  x1 <- 3
+  x2 <- 6
+  f1 <- udf_between(x1, x2)
+  f2 <- udf_between(x1, x2, "b")
+  expect_equal(typeof(f1(x1)), "logical")
+  expect_equal(typeof(f2(x1)), "logical")
+})
+
+test_that("'Between' function generates another function that works as expected", {
+  x1 <- 3
+  x2 <- 6
+
+  fn <- udf_between(x1, x2, "n")
+  expect_equal(fn(x1-1), FALSE)
+  expect_equal(fn(x1)  , FALSE)
+  expect_equal(fn(x1+1), TRUE)
+  expect_equal(fn(x2-1), TRUE)
+  expect_equal(fn(x2)  , FALSE)
+  expect_equal(fn(x2+1), FALSE)
+
+  fl <- udf_between(x1, x2, "l")
+  expect_equal(fl(x1-1), FALSE)
+  expect_equal(fl(x1)  , TRUE)
+  expect_equal(fl(x1+1), TRUE)
+  expect_equal(fl(x2-1), TRUE)
+  expect_equal(fl(x2)  , FALSE)
+  expect_equal(fl(x2+1), FALSE)
+
+  fr <- udf_between(x1, x2, "r")
+  expect_equal(fr(x1-1), FALSE)
+  expect_equal(fr(x1)  , FALSE)
+  expect_equal(fr(x1+1), TRUE)
+  expect_equal(fr(x2-1), TRUE)
+  expect_equal(fr(x2)  , TRUE)
+  expect_equal(fr(x2+1), FALSE)
+
+  fb <- udf_between(x1, x2, "b")
+  expect_equal(fb(x1-1), FALSE)
+  expect_equal(fb(x1)  , TRUE)
+  expect_equal(fb(x1+1), TRUE)
+  expect_equal(fb(x2-1), TRUE)
+  expect_equal(fb(x2)  , TRUE)
+  expect_equal(fb(x2+1), FALSE)
+})
